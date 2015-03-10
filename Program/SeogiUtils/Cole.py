609fa8d86563fe3d7eedda9082a78b4e1cd912f4
seogi <sgkang09@gmail.com>
2015-03-10 10:05:37 -0700

    Airborne IP inversion codes

---
+++ Program/SeogiUtils/Cole.py
@@ -0,0 +1,6 @@
+from SimPEG import Utils
+import numpy as np
+
+def petafun(eta, tau, t):
+    return Utils.mkvc(eta/(1-eta)/tau*np.exp(-1/(1-eta)/tau*t))
+    
---
+++ Program/SeogiUtils/__init__.py
@@ -0,0 +1,8 @@
+import View
+import BiotSavart
+import LinearIP
+import Cole
+import Linarg
+import Mumps
+import MagneticDipoleFields
+import Weights
